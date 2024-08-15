56692bc273942fcdbcf1c0f42b0c78635f9e7976
maisamir <maisamir896@gmail.com>
2024-08-15 06:28:47 -0700

    implement customer service

---
+++ customer-service/customer-dataaccess/src/main/java/com/food/ordering/system/customer/service/dataaccess/adapter/CustomerRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.food.ordering.system.customer.service.dataaccess.adapter;
+
+import com.food.ordering.system.customer.service.dataaccess.mapper.CustomerDataAccessMapper;
+import com.food.ordering.system.customer.service.dataaccess.repository.CustomerJpaRepository;
+import com.food.ordering.system.customer.service.domain.entity.Customer;
+import com.food.ordering.system.customer.service.domain.ports.output.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class CustomerRepositoryImpl implements CustomerRepository {
+
+    private final CustomerJpaRepository customerJpaRepository;
+
+    private final CustomerDataAccessMapper customerDataAccessMapper;
+
+
+    @Override
+    public Customer createCustomer(Customer customer) {
+        return customerDataAccessMapper.customerEntityToCustomer(
+                customerJpaRepository.save(customerDataAccessMapper.customerToCustomerEntity(customer)));
+    }
+}
