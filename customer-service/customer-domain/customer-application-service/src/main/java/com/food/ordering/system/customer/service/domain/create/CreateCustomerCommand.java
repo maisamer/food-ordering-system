c1c30387b4535d19250c6f715d61609cffc46802
maisamir <maisamir896@gmail.com>
2024-08-15 06:29:45 -0700

    fixing bugs

--- customer-service/customer-domain/customer-application-service/src/main/java/com/food/ordering/system/customer/service/domain/create/CreateCustomerCommand.java
+++ customer-service/customer-domain/customer-application-service/src/main/java/com/food/ordering/system/customer/service/domain/create/CreateCustomerCommand.java
@@ -1,10 +1,10 @@
 package com.food.ordering.system.customer.service.domain.create;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Getter
